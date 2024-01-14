68615af31c2b16f8d687a2f83d4fa46753d2a4c4
Ahmed Omar <eng.ahmed.omar2017@gmail.com>
2024-01-14 23:21:20 +0200

    base address

---
+++ inc/configration.h
@@ -0,0 +1,12 @@
+#ifndef CONFIGRATION_H_ 
+#define CINFIGRATION_H_
+
+#include <stdint.h>
+
+#define STM32F401           1              // Activate Definiation of stm32f401
+
+
+#define GPIO                1              // Activate ports of GPIO
+
+
+#endif/* End configration file */
---
+++ inc/macros.h
@@ -0,0 +1,19 @@
+#ifndif MACROS_H_
+#define MACROS_H_
+
+#include "configration.h"
+
+/**************************************************************************************************
+ * 
+ *                                              Generic Macros
+ * 
+****************************************************************************************************/
+
+#define ENABLE                          1
+#define DISABLE                         0
+#define SET                             ENABLE
+#define RESET                           DISABLE
+#define NO_PR_BITS_IMPLEMENTED          4
+
+
+#endif /* End macros file */
