81b523655cf6e6cf67c6aa61a36dffdee765b348
Ahmed Omar <eng.ahmed.omar2017@gmail.com>
2024-01-16 21:50:40 +0200

    add if condition at macro file

--- inc/macros.h
+++ inc/macros.h
@@ -53,64 +53,64 @@
 
 
 
+#if STM32F401 == 1
 
 
 
-
-   /*
-   * @ GPIO_PIN_MODES
-   * Mode Configration 
-   * 
-   * */
-
-
-
-   #define GPIO_MODE_IN                0
-   #define GPIO_MODE_OUT               1
-   #define GPIO_MODE_ALTFN             2
-   #define GPIO_MODE_ANALOG            3
-   #define GPIO_MODE_IT_FT             4
-   #define GPIO_MODE_IT_RT             5
-   #define GPIO_MODE_IT_RFT            6
+    /*
+    * @ GPIO_PIN_MODES
+    * Mode Configration 
+    * 
+    * */
 
 
 
-   /*
-   *  @Type of output
-   * output type Configration 
-   * 
-   * */
+    #define GPIO_MODE_IN                0
+    #define GPIO_MODE_OUT               1
+    #define GPIO_MODE_ALTFN             2
+    #define GPIO_MODE_ANALOG            3
+    #define GPIO_MODE_IT_FT             4
+    #define GPIO_MODE_IT_RT             5
+    #define GPIO_MODE_IT_RFT            6
 
 
 
-   #define GPIO_OP_TYPE_PP            0
-   #define GPIO_OP_TYOE_PP            1
+    /*
+    *  @Type of output
+    * output type Configration 
+    * 
+    * */
 
 
 
-   /*
-   * @ GPIO_PIN_SPEED register 
-   * output speed Configration 
-   * 
-   * */
+    #define GPIO_OP_TYPE_PP            0
+    #define GPIO_OP_TYOE_PP            1
 
-   #define GPIO_SPEED_LOW           0      
-   #define GPIO_SPEED_MEDIUM        1
-   #define GPIO_SPEED_FAST          2
-   #define GPIO_SPEED_HIGH          3
 
-   /*
-   * @pullup and pulldown
-   * Pull up and pull down Configration 
-   * 
-   * */
 
-   #define GPIO_NO_PUPD              0
-   #define GPIO_PIN_PU               1
-   #define GPIO_PIN_PD               2
+    /*
+    * @ GPIO_PIN_SPEED register 
+    * output speed Configration 
+    * 
+    * */
 
-   
+    #define GPIO_SPEED_LOW           0      
+    #define GPIO_SPEED_MEDIUM        1
+    #define GPIO_SPEED_FAST          2
+    #define GPIO_SPEED_HIGH          3
 
+    /*
+    * @pullup and pulldown
+    * Pull up and pull down Configration 
+    * 
+    * */
+
+    #define GPIO_NO_PUPD              0
+    #define GPIO_PIN_PU               1
+    #define GPIO_PIN_PD               2
+
+    
+    #endif/*end STM32F401*/
 
 
 
